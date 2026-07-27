78974911ca992da53832fa3f31bee1d043598e49
HelgiPoslannik <podcasthelgi@gmail.com>
2026-07-27 23:04:54 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,11 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+COZE_TOKEN = os.getenv("COZE_TOKEN")
+
+COZE_BOT_ID = os.getenv("COZE_BOT_ID")
+
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+CACHE_TIME = 300
